0d79fc9d615418c7d49ef83bc36741ee9e3fd92e
Lucas Souza <42921327+lucas2179@users.noreply.github.com>
2020-09-10 15:01:06 -0300

    Update getinfo.sh

--- getinfo.sh
+++ getinfo.sh
@@ -6,6 +6,6 @@ for i in $(ibmcloud account list |  awk '{print $1}' | grep -v -e "Retrieving" -
 do
     ibmcloud target -c $i
     ibmcloud account show 
-    idrec = ibmcloud sl call-api SoftLayer_Account getLatestRecurringInvoice | jq '.id'
-    ibmcloud sl call-api SoftLayer_Billing_Invoice getInvoiceTopLevelItems --init $idrec --mask 'mask[ id, description, hostName, domainName, oneTimeAfterTaxAmount, recurringAfterTaxAmount, createDate, categoryCode, category[name], location[name] ]' | jq -r '.[] | [.recurringAfterTaxAmount, .categoryCode, .description, .hostName] | @csv' | awk -v FS="," 'BEGIN{print "Cost\tCategory\tDescription\thostName"}{printf "%s\t%s\t%20s\t%s%s", $1, $2, $3, $4, ORS}' | column -s $'\t' -t
+    IDREC = ibmcloud sl call-api SoftLayer_Account getLatestRecurringInvoice | jq '.id'
+    ibmcloud sl call-api SoftLayer_Billing_Invoice getInvoiceTopLevelItems --init $IDREC --mask 'mask[ id, description, hostName, domainName, oneTimeAfterTaxAmount, recurringAfterTaxAmount, createDate, categoryCode, category[name], location[name] ]' | jq -r '.[] | [.recurringAfterTaxAmount, .categoryCode, .description, .hostName] | @csv' | awk -v FS="," 'BEGIN{print "Cost\tCategory\tDescription\thostName"}{printf "%s\t%s\t%20s\t%s%s", $1, $2, $3, $4, ORS}' | column -s $'\t' -t
 done
